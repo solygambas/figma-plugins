6204ca1f12731a564be8dab822194feb8fb2145e
solygambas <51904909+solygambas@users.noreply.github.com>
2022-06-01 13:31:36 +0200

    add custom inputs

--- 04-social-media-post/README.md
+++ 04-social-media-post/README.md
@@ -2,11 +2,20 @@
 
 A plugin that creates a customizable post for a social media app design.
 
+<p align="center">
+    <img src="screenshot.png">
+</p>
+
 ## Features
 
 - building a UI with the Figma Plugin API.
 - using [Tom Lowry's UI library](https://github.com/thomas-lowry/figma-plugin-ds).
 - creating an instance of a variant from a [component set](https://www.figma.com/community/file/915647337333327091).
 - taking user input and using it to create new components in a Figma file.
+- adding a dark mode conditional.
+- selecting the text nodes and overwriting the default text.
+- randomizing comments and likes.
+- adding the current time and date.
+- handling scroll and zoom info view.
 
 Based on [Build your first plugin](https://www.youtube.com/playlist?list=PLXDU_eVOJTx5YBAszyuOTyxlgIxkQVyii) by Figma (2022).
--- 04-social-media-post/code.ts
+++ 04-social-media-post/code.ts
@@ -1,7 +1,103 @@
 figma.showUI(__html__);
 figma.ui.resize(500, 500);
 
-figma.ui.onmessage = (pluginMessage) => {
-  console.log(pluginMessage.name);
+figma.ui.onmessage = async (pluginMessage) => {
+  // add error handling
+  await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
+
+  const nodes: SceneNode[] = [];
+
+  const postComponentSet = figma.root.findOne(
+    (node) => node.type == "COMPONENT_SET" && node.name == "post"
+  ) as ComponentSetNode;
+  //const defaultDark = postComponentSet.defaultVariant as ComponentNode;
+  let selectedVariant;
+  if (pluginMessage.darkModeState) {
+    switch (pluginMessage.imageVariant) {
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=carousel, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=single, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=none, Dark mode=true"
+        ) as ComponentNode;
+        break;
+    }
+  } else {
+    switch (pluginMessage.imageVariant) {
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=carousel, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=single, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=none, Dark mode=false"
+        ) as ComponentNode;
+        break;
+    }
+  }
+  const newPost = selectedVariant.createInstance();
+  const templateName = newPost.findOne(
+    (node) => node.name == "displayName" && node.type == "TEXT"
+  ) as TextNode;
+  const templateUsername = newPost.findOne(
+    (node) => node.name == "@username" && node.type == "TEXT"
+  ) as TextNode;
+  const templateNDescription = newPost.findOne(
+    (node) => node.name == "description" && node.type == "TEXT"
+  ) as TextNode;
+
+  const numLikes = newPost.findOne(
+    (node) => node.name == "likesLabel" && node.type == "TEXT"
+  ) as TextNode;
+  const numComments = newPost.findOne(
+    (node) => node.name == "commentsLabel" && node.type == "TEXT"
+  ) as TextNode;
+
+  const timestamp = newPost.findOne(
+    (node) => node.name == "timestamp" && node.type == "TEXT"
+  ) as TextNode;
+  const datestamp = newPost.findOne(
+    (node) => node.name == "datestamp" && node.type == "TEXT"
+  ) as TextNode;
+  let date = new Date().toLocaleString("fr-FR");
+  const [today, now] = date.split(",");
+
+  timestamp.characters = now;
+  datestamp.characters = today;
+
+  templateName.characters = pluginMessage.name;
+  templateNDescription.characters = pluginMessage.description;
+  templateUsername.characters = pluginMessage.username;
+  numLikes.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+  numComments.characters = (Math.floor(Math.random() * 1000) + 1).toString();
+
+  nodes.push(newPost);
+  figma.viewport.scrollAndZoomIntoView(nodes);
   figma.closePlugin();
 };
